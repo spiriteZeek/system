5dcaf7082d117946b3727d772c2e9790ecf29516
spiriteZeek <zeek323474@gmail.com>
2024-04-09 16:42:26 +0800

    feat:上传图片

--- server/node-server.js
+++ server/node-server.js
@@ -1,10 +1,24 @@
 import http from 'node:http'
+import fs from 'node:fs'
 
 const server = http.createServer((req, res) => {
+  let fileBase64 = ''
+  
+  if (req.method.toLowerCase() === 'post') {
+    console.log(req.method)
+    req.on('data', (chunk) => {
+      fileBase64 += chunk.toString()
+    })
+    req.on('end', () => {
+      const buf = Buffer.from(JSON.parse(fileBase64).file, 'base64')
+      fs.writeFileSync('test.png', buf)
+    })
+  }
+
   res.writeHead(200, {
     token: '213zsdxf',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'zgy',
+    'Access-Control-Allow-Headers': 'zgy,content-type',
     'Access-Control-Allow-Methods': 'PUT'
   })
   res.end('hello')
--- src/components/biz/Myfile.vue
+++ src/components/biz/Myfile.vue
@@ -1,14 +1,16 @@
 <template>
   <el-button @click="upload">选择文件</el-button>
   <input @change="handleChange" ref="file" id="fileInput" type="file" />
+  <el-tag id="filename" type="primary" v-show="filename">{{ filename }}</el-tag>
   <div>
-    <el-tag id="filename" type="primary" v-show="filename">{{ filename }}</el-tag>
+    <el-button @click="testServer">上传文件</el-button>
+    <el-button @click="base64Upload">base64上传</el-button>
   </div>
-  <el-button @click="testServer">上传文件</el-button>
+  <img v-show="filename" :src="imgUrl" />
 </template>
 
 <script>
-import { ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import { selfServer } from '@/http/axios.js'
 export default {
   name: 'MyfileComp',
@@ -16,15 +18,38 @@ export default {
     const file = ref(null)
     const filename = ref('')
     const formData = new FormData()
+    const imgUrl = ref('')
+    const base64Reader = new FileReader()
+    function getReaderResult() {
+      imgUrl.value = base64Reader.result
+    }
+    onMounted(() => {
+      base64Reader.addEventListener('load', getReaderResult)
+    })
+
+    onUnmounted(() => {
+      base64Reader.addEventListener('load', getReaderResult)
+    })
+
     function handleChange(e) {
       console.log(e.target.files)
       console.log(file.value.files)
       filename.value = file.value.files[0].name
       formData.append('file', file.value.files[0])
+      base64Reader.readAsDataURL(file.value.files[0])
     }
 
+    // formdat表单上传文件
     function testServer() {
       selfServer.put('/upload', formData)
+    }
+
+    // base64编码
+    function base64Upload() {
+      const fileBase64 = imgUrl.value.split(',')[1]
+      const zgyBase64 = imgUrl.value
+      console.log(imgUrl.value.split(',')[1])
+      selfServer.post('/upload', {file: fileBase64})
     }
 
     function upload() {
@@ -35,7 +60,9 @@ export default {
       upload,
       handleChange,
       filename,
-      testServer
+      testServer,
+      imgUrl,
+      base64Upload
     }
   }
 }
@@ -48,5 +75,8 @@ export default {
 }
 #filename {
   margin-top: 10px;
+}
+img {
+  width: 300px;
 }
 </style>
